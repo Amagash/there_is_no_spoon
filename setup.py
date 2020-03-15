ff7234fd103bf97f0914c3569885ded2d01a7288
Amagash <tiffanysouterre@gmail.com>
2020-03-15 12:18:10 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='there_is_no_spoon',
+    author='Tiffany Souterre',
+    version='1.0.0',
+    packages=['there_is_no_spoon'],
+    description="An application to generate adversarial examples",
+    entry_points={
+        'console_scripts': ['there_is_no_spoon=there_is_no_spoon.main:main']
+    }
+)

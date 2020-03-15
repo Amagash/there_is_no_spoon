7b4e81fcd76c7eb49efccf600d828586798b2725
Amagash <tiffanysouterre@gmail.com>
2020-03-15 17:40:29 +0100

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='there_is_no_spoon',
     author='Tiffany Souterre',
-    version='1.0.1',
+    version='1.0.2',
     packages=['there_is_no_spoon'],
+    package_data={'there_is_no_spoon': ['imagenet_class_index.json']},
+    include_package_data=True,
     description="An application to generate adversarial examples",
     entry_points={
         'console_scripts': ['there_is_no_spoon=there_is_no_spoon.main:main']

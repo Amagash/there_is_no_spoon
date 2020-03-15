2a9c1219ecc5985781ccfef3b81b652e00692197
Amagash <tiffanysouterre@gmail.com>
2020-03-15 20:50:18 +0100

    erased json in setup

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ setup(
     author='Tiffany Souterre',
     version='1.0.7',
     packages=['there_is_no_spoon'],
-    package_data={'there_is_no_spoon': ['imagenet_class_index.json']},
-    include_package_data=True,
     description="An application to generate adversarial examples",
     entry_points={
         'console_scripts': ['there_is_no_spoon=there_is_no_spoon.main:main']

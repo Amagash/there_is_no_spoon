5e99dca8dab5ff0955ff0610df94af3437a48f54
Amagash <tiffanysouterre@gmail.com>
2020-03-20 00:32:26 +0100

    add requirements in setup and changed version

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+from pkg_resources import parse_requirements
 from setuptools import setup
+
+requirements = parse_requirements('requirements.txt')
 
 setup(
     name='there_is_no_spoon',
     author='Tiffany Souterre',
-    version='1.0.11',
+    version='1.0.12',
+    install_requires=requirements,
     packages=['there_is_no_spoon'],
     description="An application to generate adversarial examples",
     entry_points={
